svn2github/wxWidgets

commit 8d8306544413cfa82d39a2f0b2efa03db98a5beb

Fix for wxStringCheck compilation under IRIX using mipsPro.
IRIX mipsPro 7.4 refuses to instantiate a template with an inline function as
parameter. Work around this by using a function taking a function pointer
instead of using template wxStringCheck with inline wxIs{alpha,alnum,digit}.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63582 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/common/valtext.cpp
@@ -201,17 +201,38 @@ bool wxTextValidator::TransferFromWindow()
     return true;
 }
 
+// IRIX mipsPro refuses to compile wxStringCheck<func>() if func is inline so
+// let's work around this by using this non-template function instead of
+// wxStringCheck(). And while this might be fractionally less efficient because
+// the function call won't be inlined like this, we don't care enough about
+// this to add extra #ifs for non-IRIX case.
+namespace
+{
+
+bool CheckString(bool (*func)(const wxUniChar&), const wxString& str)
+{
+    for ( wxString::const_iterator i = str.begin(); i != str.end(); ++i )
+    {
+        if ( !func(*i) )
+            return false;
+    }
+
+    return true;
+}
+
+} // anonymous namespace
+
 wxString wxTextValidator::IsValid(const wxString& val) const
 {
     // wxFILTER_EMPTY is checked for in wxTextValidator::Validate
 
     if ( HasFlag(wxFILTER_ASCII) && !val.IsAscii() )
         return _("'%s' should only contain ASCII characters.");
-    if ( HasFlag(wxFILTER_ALPHA) && !wxStringCheck<wxIsalpha>(val) )
+    if ( HasFlag(wxFILTER_ALPHA) && !CheckString(wxIsalpha, val) )
         return _("'%s' should only contain alphabetic characters.");
-    if ( HasFlag(wxFILTER_ALPHANUMERIC) && !wxStringCheck<wxIsalnum>(val) )
+    if ( HasFlag(wxFILTER_ALPHANUMERIC) && !CheckString(wxIsalnum, val) )
         return _("'%s' should only contain alphabetic or numeric characters.");
-    if ( HasFlag(wxFILTER_DIGITS) && !wxStringCheck<wxIsdigit>(val) )
+    if ( HasFlag(wxFILTER_DIGITS) && CheckString(wxIsdigit, val) )
         return _("'%s' should only contain digits.");
     if ( HasFlag(wxFILTER_NUMERIC) && !wxIsNumeric(val) )
         return _("'%s' should be numeric.");

commit 00353e9468cafc7ebad2a8b7bf744c4048cce59b

Correct wxFILTER_DIGITS handling broken by r63582.
The fix in r63582 accidentally omitted the "!" in wxFILTER_DIGITS check.

Restore it.

Closes #11838.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@63732 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/common/valtext.cpp
@@ -232,7 +232,7 @@ wxString wxTextValidator::IsValid(const wxString& val) const
         return _("'%s' should only contain alphabetic characters.");
     if ( HasFlag(wxFILTER_ALPHANUMERIC) && !CheckString(wxIsalnum, val) )
         return _("'%s' should only contain alphabetic or numeric characters.");
-    if ( HasFlag(wxFILTER_DIGITS) && CheckString(wxIsdigit, val) )
+    if ( HasFlag(wxFILTER_DIGITS) && !CheckString(wxIsdigit, val) )
         return _("'%s' should only contain digits.");
     if ( HasFlag(wxFILTER_NUMERIC) && !wxIsNumeric(val) )
         return _("'%s' should be numeric.");